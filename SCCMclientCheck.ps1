48e319ee81107bd91cb864fdf9303c6f302191ab
ADGuarino <35603329+ADGuarino@users.noreply.github.com>
2018-01-22 09:49:12 -0800

    Add files via upload

---
+++ SCCMclientCheck.ps1
@@ -0,0 +1,18 @@
+﻿$a = new-object -comobject wscript.shell 
+[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') 
+$Computer = [Microsoft.VisualBasic.Interaction]::InputBox('Enter the Computer Name', 'SCCM Client Check')
+
+$Service = Get-Service -Name 'sms agent host' -ComputerName $Computer -ErrorAction SilentlyContinue
+if ($service.Status -eq "Running") {
+$a.popup("SCCM Client is installed and healthy on $Computer")
+Exit
+}
+Else {
+$a.popup("$Computer is missing SCCM Client.")
+Exit
+}
+
+
+
+
+
